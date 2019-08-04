11677b6b2b39c3f998a5eafd02e8335cd3181b4d
Michael Gacek <gacek.michael@outlook.com>
2019-08-05 01:24:04 +0200

    Added basically (CRUD) operations to model

--- src/Model.js
+++ src/Model.js
@@ -1,3 +1,32 @@
-class Model {}
+class Model {
+  constructor() {
+    this.todos = [
+      { id: 1, text: "Create a new JS app", completed: false },
+      { id: 2, text: "Test your new app", completed: false }
+    ];
+  }
+
+  addTodo(todo) {
+    this.todos = [...this.todos, todo];
+  }
+
+  editTodo(id, text) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id ? { id: todo.id, text, completed: todo.completed } : todo
+    );
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id
+        ? { id: todo.id, text: todo.text, completed: !todo.completed }
+        : todo
+    );
+  }
+}
 
 export default Model;
